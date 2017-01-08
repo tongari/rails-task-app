c955a17e37a9e3bb0f8ceb91ea5f2069037f21dd
tongari <info@tongari-as.com>
2017-01-09 00:43:11 +0900

    mecha tocyu

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,25 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    # @project = Project.new(params[:project])
+    @project.save
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params[:project].permit(:title)
+    end
+end
