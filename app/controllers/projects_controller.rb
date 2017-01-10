96f9bc4e4377887ca01b32788a4669ea88775318
tongari <info@tongari-as.com>
2017-01-11 00:09:25 +0900

    update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,11 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only:[:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -18,11 +19,30 @@ class ProjectsController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
     def project_params
       params[:project].permit(:title)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
     end
 end
