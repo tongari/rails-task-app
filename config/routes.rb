418738c8c05cbd48f740f51931db3f13371df007
tongari <info@tongari-as.com>
2017-02-02 01:13:57 +0900

    make asynchronous post

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tasks, only: [:create, :destroy]
   end
+  post 'projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+
   root 'projects#index'
 end
